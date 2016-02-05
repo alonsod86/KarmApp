04bd10a008102522b3f6e728295fab4418278a11
Daniel Gutierrez <Daniel Gutierrez>
2016-02-05 12:09:35 +0100

    termómetros

--- app/src/main/java/fs/ndt/karmapp/CardsActivity.java
+++ app/src/main/java/fs/ndt/karmapp/CardsActivity.java
@@ -59,14 +59,14 @@ public class CardsActivity extends AppCompatActivity
         mAdapter = new CardAdapter(loadAllEvents());
         mRecyclerView.setAdapter(mAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
